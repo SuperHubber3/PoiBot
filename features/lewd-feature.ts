971b7f0b620835a072be0f4707f1e588f533584c
SuperHubber3 <dogan.ejanberk@gmail.com>
2022-08-27 11:47:38 +0300

    stop crying lol

--- features/lewd-feature.ts
+++ features/lewd-feature.ts
@@ -1,16 +1,16 @@
 import type { Client, Sticker } from "discord.js";
 const guildId = "735499671648206889"
-const cooldown = new Set();
+const set = new Set();
 
 export default (client: Client) => {
     client.on("messageCreate", async (message) => {
         if (guildId !== message.guildId) return
-        if (cooldown.has(true)) return
+        if (set.has(true)) return
         const content = message.content;
-        if (content.toLowerCase().includes("lewd") || content.toLowerCase().includes("lood")) {
-            cooldown.add(true);
+        if ((content.toLowerCase().includes("lewd") || content.toLowerCase().includes("lood")) && !content.toLowerCase().includes("blood")) {
+            set.add(true);
             setTimeout(() => {
-                cooldown.delete(true)
+                set.delete(true)
             }, 1000 * 60 * 60);
             message.reply({
                 stickers: [client.guilds.cache.get(guildId)?.stickers.cache.get("1008452255118262312") as Sticker],
