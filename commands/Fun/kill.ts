f79c75e9d24002fe7910c38f994fda43d85636d0
SuperHubber3 <64640383+SuperHubber3@users.noreply.github.com>
2022-11-01 09:41:11 +0300

    Update kill.ts

--- commands/Fun/kill.ts
+++ commands/Fun/kill.ts
@@ -26,7 +26,7 @@ export default {
             interactionUser = interactionUser.slice(0, -1)
         }
         let target: string = interactionUser
-        let targetName: string = msgInt?.options.getUser("user")?.username!
+        let targetName: string = msgInt?.options.getUser("user")?.username! || client.users.fetch(interactionUser)
 
         if (args[0] == "me") {
             const embed = new MessageEmbed()
